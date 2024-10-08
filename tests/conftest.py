0706edbce3e9fb66ece8e66b912a15a5c53dae10
Stephen Finucane <stephen@that.guru>
2024-10-08 17:57:36 +0100

    Remove use of deprecated util
    Signed-off-by: Stephen Finucane <stephen@that.guru>

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,10 +6,10 @@
     :license: BSD, see LICENSE for details.
 """
 
+import pathlib
 import shutil
 
 import pytest
-from sphinx.testing import path
 
 # this is necessary because Sphinx isn't exposing its fixtures
 # https://docs.pytest.org/en/7.1.x/how-to/writing_plugins.html#requiring-loading-plugins-in-a-test-module-or-conftest-file
@@ -18,9 +18,9 @@ pytest_plugins = 'sphinx.testing.fixtures'
 
 @pytest.fixture
 def rootdir(tmpdir):
-    src = path.path(__file__).parent.abspath() / 'roots'
+    src = pathlib.Path(__file__).parent.resolve() / 'roots'
     dst = tmpdir.join('roots')
     shutil.copytree(src, dst)
-    roots = path.path(dst)
+    roots = pathlib.Path(dst)
     yield roots
     shutil.rmtree(dst)
